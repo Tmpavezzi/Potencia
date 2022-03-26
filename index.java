9091f911fa786404779e4806550efeee8d829954
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-03-25 21:49:13 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,28 @@
+exer 02                                                                               
+import java.lang.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    double n1, n2 ;
+    System.out.println("Escreva um numero ");
+    n1 = scanner.nextInt();
+    System.out.println("Escreva um numero ");
+    n2 = scanner.nextInt();             
+
+     Segundo Modo 
+
+import java.util.*;
+import java.lang.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    double n1, n2 ;
+    System.out.println("Escreva um numero ");
+    n1 = scanner.nextInt();
+    System.out.println("Escreva um numero ");
+    n2 = scanner.nextInt();
+    double Potencia = Math.pow(n1 , 2 )+ Math.pow(n2 , 2 );
+    System.out.println("Resultado " + Potencia);
+
